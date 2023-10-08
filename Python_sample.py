3045eb6de7a691f48b7aff499e94574d3092d4d9
Adwaith R Babu <adwaithktk@gmail.com>
2023-10-08 23:50:45 +0530

    new

--- Python_sample.py
+++ Python_sample.py
@@ -1,3 +1,11 @@
-a = "adwaith"
-s = a[-3:]
-print(s)
+n = int(input())
+year = n / 365
+years_int = int(year)
+weeks = n - (years_int * 365)
+weeks_in = weeks / 7
+weeks_int = int(weeks_in)
+days = weeks_in - (weeks_int * 7)
+days_int = int(days)
+print(years_int)
+print(weeks_int)
+print(days_int)
